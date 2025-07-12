91b02fd0621312457422cf272ee4d35308c01b60
Bakyt <bakosyy@gmail.com>
2025-07-13 00:40:42 +0500

    feat: remove crossorigin

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,15 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
+const noCrossOrigin = () => {
+  return {
+    name: 'no-crossorigin',
+    transformIndexHtml(html) {
+      return html.replace(/crossorigin /g, '')
+    },
+  }
+}
+
 export default defineConfig({
-  plugins: [tailwindcss()],
+  plugins: [tailwindcss(), noCrossOrigin()],
 })
